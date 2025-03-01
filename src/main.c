7169d8c380bf14e43973895c8dc724b7993aaac2
KILI-BC <45309327+KILI-BC@users.noreply.github.com>
2025-03-01 16:26:53 +0100

    Implemented calc_next()

--- src/main.c
+++ src/main.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <time.h>
 
+/*has to be at least 3 for the code to work*/
 #define TAPE_WIDTH 16
+#define WOLFRAM_CODE 30
+#define EVALUATE_CODE(c) ((WOLFRAM_CODE >> (c)) & 1)
 typedef int tape;
 
 tape get_random();
@@ -14,7 +17,12 @@ int main()
 	tape t;
 	srand(time(NULL));
 	t = get_random();
-	print_tape(t);
+	while (1)
+	{
+		print_tape(t);
+		t = calc_next(t);
+	}
+	
 	return EXIT_SUCCESS;
 }
 
@@ -27,7 +35,19 @@ tape get_random()
 
 tape calc_next(tape t)
 {
-	return t;
+	int i;
+	tape ret = 0;
+
+	/*handle leftmost bit*/
+	ret |= EVALUATE_CODE(((t & 1) << 2) | ((t >> (TAPE_WIDTH - 2))& 3)) << (TAPE_WIDTH - 1);
+
+	/*handle middle bits*/
+	for (i = (TAPE_WIDTH - 2); i > 0; i++)
+		ret |= (EVALUATE_CODE((t >> (i - 1)) & 7) << i);
+
+	/*handle rightmost bit*/
+	ret |= EVALUATE_CODE(((t & 3) << 1) | (t >> (TAPE_WIDTH - 1)));
+	return ret;
 }
 
 void print_tape(tape t)
