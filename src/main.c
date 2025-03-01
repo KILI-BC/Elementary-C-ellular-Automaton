0837d36cdcc7b6862f5bbdd9056548230a36f534
KILI-BC <45309327+KILI-BC@users.noreply.github.com>
2025-03-01 12:36:32 +0100

    Implemented + tested some functions
    - Renamed band to tape
    - Implemented get_random()
    - Implemented print_tape()

--- src/main.c
+++ src/main.c
@@ -1,23 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-#define TAPE_WIDTH 32
+#define TAPE_WIDTH 16
 typedef int tape;
 
-tape calc_next(tape b);
-void print_band(tape b);
+tape get_random();
+tape calc_next(tape t);
+void print_tape(tape t);
 
 int main()
 {
+	tape t;
+	srand(time(NULL));
+	t = get_random();
+	print_tape(t);
 	return EXIT_SUCCESS;
 }
 
-tape calc_next(tape b)
+tape get_random()
 {
-
+	tape t;
+	t = rand() % (1 << TAPE_WIDTH);
+	return t;
 }
 
-void print_band(tape b)
+tape calc_next(tape t)
 {
+	return t;
+}
 
+void print_tape(tape t)
+{
+	int i;
+	for (i = (TAPE_WIDTH - 1); i >= 0; i--)
+		printf("%i", (t >> i) & 1);
+	putchar('\n');
 }
