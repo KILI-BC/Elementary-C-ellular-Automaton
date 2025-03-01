0fd5ac23647d551a98a328bd6fa43d14d6f0176e
KILI-BC <45309327+KILI-BC@users.noreply.github.com>
2025-03-01 12:25:50 +0100

    Added basic program structure

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TAPE_WIDTH 32
+typedef int tape;
+
+tape calc_next(tape b);
+void print_band(tape b);
+
+int main()
+{
+	return EXIT_SUCCESS;
+}
+
+tape calc_next(tape b)
+{
+
+}
+
+void print_band(tape b)
+{
+
+}
